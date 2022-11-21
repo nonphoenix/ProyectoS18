1f6bee2bfcadf00eabf10372d52cb7c2cc552e6d
nonphoenix <118759761+nonphoenix@users.noreply.github.com>
2022-11-21 12:42:32 -0500

    Update funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,6 @@
-
+function sumar() {
+  const numero1 = document.getElementById("num1").value;
+  const numero2 = document.getElementById("num2").value;
+  let sum = parseInt(numero1) + parseInt(numero2);
+  document.getElementById("resultado").innerHTML = sum;
+}
