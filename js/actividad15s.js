d32ef9ab1dc6de9d34e2497066e14b97ffeacecf
nonphoenix <118759761+nonphoenix@users.noreply.github.com>
2022-11-22 04:05:05 -0500

    Create actividad15s.js

---
+++ js/actividad15s.js
@@ -0,0 +1,43 @@
+let c;
+c = parseInt(prompt("Ingresa tu importe de compra",''));
+p = c * 0.12;
+d = c - p;
+if (c >= 150) {
+document.write(
+"Importe: S/" +
+c +
+"<br>" +
+"Descuento: S/" +
+p +
+"<br>" +
+"Importe final: S/" +
+d
+);
+} else {
+document.write(
+"Importe: S/" +
+c +
+"<br>" +
+"No tiene descuento: S/0" +
+"<br>" +
+"Importe final: S/" +
+c
+);
+}
+let nombre;
+nombre = prompt("Ingrese su nombre", "");
+let ep;
+ep = parseInt(prompt("Ingresa tu nota de examen parcial", ""));
+let ef;
+ef = parseInt(prompt("Ingresa tu nota de examen final", ""));
+let pp;
+pp = parseInt(prompt("Ingresa tu nota de promedio de prácticas",
+""));
+pesoep = ep * 0.25;
+pesoef = ef * 0.5;
+pesopp = pp * 0.25;
+promedioFinal = pesoep + pesoef + pesopp;
+document.write("Hola, " + nombre + "<br>");
+if (promedioFinal >= 12) {
+document.write("Su promedio final es: " + promedioFinal);
+}
